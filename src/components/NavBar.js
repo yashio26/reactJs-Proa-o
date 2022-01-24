5552c212bb4115c7c62fd5a7abb179fdfd8850b0
Dario Proaño <yashio200007@gmail.com>
2022-01-24 18:39:29 -0300

    Agrego un navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (        
+<nav className="navigation">
+      <ul>
+          <li>
+              <a href="../public/index.html">Personajes</a>
+          </li>
+          <li>
+              <a href="../public/index.html">Skins</a>
+          </li>
+          <li>
+              <a href="../public/index.html">Celulas auricas</a>
+          </li>
+      </ul>
+</nav>);
+};
+
+export default NavBar;
