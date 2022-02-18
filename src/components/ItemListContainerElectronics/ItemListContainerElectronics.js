47278ff0e81b2bac0c2eafb9f5d62728ed821347
Dario Proaño <yashio200007@gmail.com>
2022-02-18 14:31:33 -0300

    Agrego Spinners y mejoro css

--- src/components/ItemListContainerElectronics/ItemListContainerElectronics.js
+++ src/components/ItemListContainerElectronics/ItemListContainerElectronics.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect} from 'react';
 import Item from '../Item/Item';
-import '../ItemList/ItemList.css';
+import './ItemListContainerElectronics.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
 
 const ItemListContainerClothes = () => {    
     const [electronics, setElectronics] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         axios(`${process.env.REACT_APP_BASE_URL}products/category/electronics`).then((res) => setElectronics(res.data));
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
         }, []);
 
     return (
     <div className="Articulos">
-        {electronics.map((electronic) => {
-            return(
-                <div key={electronic.id} className='Articulo'>
-                    <Link to={`/item/${electronic.id}`}>
-                        <Item data={electronic}/>
-                    </Link>
-                </div>
-            );
-        })}
+        {isLoading ? (
+				<Spinner />
+			) : ( 
+                <>
+                    {electronics.map((electronic) => {
+                        return(
+                            <div key={electronic.id} className='Articulo'>
+                                <Link to={`/item/${electronic.id}`}>
+                                    <Item data={electronic}/>
+                                </Link>
+                            </div>
+                        );
+                    })}
+                </>
+            )
+        }
     </div>
     );
 };
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 
 export default function Spinner() {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex', margin: 2 }}>
       <CircularProgress />
     </Box>
   );
